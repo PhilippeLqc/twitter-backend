34c5b380badb72c629962176c12d5a9e97181b6d
alexsuire <alexandresuire75002@gmail.com>
2023-04-27 16:51:41 +0200

    yo

--- models/tweets.js
+++ models/tweets.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const tweetSchema = mongoose.Schema({
   date: Date,
   message: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+  like: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }]
 });
 
 const Tweet = mongoose.model('tweets', tweetSchema);
