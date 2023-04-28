d59f757d0926e0cf5c1113e9365374ca2bd26c4b
Philippe Le Quoc <philippe.lequoc@gmail.com>
2023-04-28 09:01:23 +0200

    trololo

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+// const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://philippelacapsule:H1r0nd3773@cluster0.6dvqohn.mongodb.net/hackatweet'
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,6 @@ router.post('/signup', (req, res) => {
         username: req.body.username,
         password: hash,
         token: uid2(32),
-        tweets: { type: mongoose.Schema.Types.ObjectId, ref: 'tweets' },
 
       });
 
