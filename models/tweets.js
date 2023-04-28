dd815895bfcc682c456af7d5f60692345b8aa3a0
Philippe Le Quoc <philippe.lequoc@gmail.com>
2023-04-28 12:25:41 +0200

    essai1hashtag

--- models/tweets.js
+++ models/tweets.js
@@ -4,7 +4,8 @@ const tweetSchema = mongoose.Schema({
   date: Date,
   message: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-  like: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }]
+  like: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+  hashtags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'hashtags'}]
 });
 
 const Tweet = mongoose.model('tweets', tweetSchema);
